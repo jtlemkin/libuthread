f96ea9de94d13c18d15e6810743a043d845cdbe4
Nicholas Treynor <Ntreynor@gmail.com>
2020-11-02 15:10:49 -0800

    Realising this development environment is still not ideal, not receiving error messages from compiler. More functionality added to queue.

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -7,7 +7,7 @@
 
 struct queueNode {
     /* TODO Phase 1 */
-    int data; // Perhaps we need to replace this at a later date? Should be fine for now.
+    void* data; // Perhaps we need to replace this at a later date? Should be fine for now. We need to ensure this can store all data types.
     queue* nextNode;
 };
 
@@ -22,9 +22,9 @@ queue_t queue_create(void)
 {
 	/* TODO Phase 1 */
 	// Will need to malloc data for the queue.
-	struct queue* Cue = (struct queue*) malloc* sizeof((struct queue));
-	Cue -> headNode = NULL;
-	Cue -> tailNode = NULL;
+	struct queue* newQueue = (struct queue*) malloc* sizeof((struct queue));
+    newQueue -> headNode = NULL;
+    newQueue -> tailNode = NULL;
 	return Cue;
 
 	//TODO: Return "NULL" if malloc fails
@@ -34,16 +34,55 @@ queue_t queue_create(void)
 int queue_destroy(queue_t queue)
 {
 	/* TODO Phase 1 */
+	// Involves freeing up all the data in the queue.
 }
 
 int queue_enqueue(queue_t queue, void *data)
 {
 	/* TODO Phase 1 */
+
+	// Create a new node:
+    struct queueNode* newNode = (struct queueNode*) malloc* sizeof((struct queueNode));
+    newNode->data = &data; // Set the data pointer of the node to point at the address of the input data. (Only way I can think to handle void* style data input)
+                             // May need to reference the discussion from last week to ensure this is working properly. Proper approach might be: newNode.data = &data;
+	newNode->nextNode = NULL;
+
+	// Then add it to the queue:
+	if (queue->headNode != NULL){ // If containing nodes already, set the tail's next node to be equal to the newly created node, then set the new node as the new tail
+	    queue->tailNode->nextNode = newNode;
+	    queue->tailNode = newNode;
+	} else{ // If we need to initialize the queue with the first bit of data, we have to set both the head and tail equal to the same new node.
+        queue->tailNode = newNode;
+        queue->headNode = newNode;
+	}
+
+	//TODO: appropriate error codes on return.
+	return 0;
+
 }
 
 int queue_dequeue(queue_t queue, void **data)
 {
 	/* TODO Phase 1 */
+
+    if (queue->headNode == NULL){
+        return -1; // Cannot dequeue, queue is empty.
+    }
+
+
+	*data = queue->headNode.data; // update the void pointer to point to the data being stored. Somewhat uncertain about this due to void** shennanigans
+
+	struct queueNode tempHead = queue->headNode; // Store old head temporarily
+	queue->headNode = queue->headNode->nextNode; // Adjust queue to account for removal of the old head
+
+	if (queue->headNode == NULL){
+	    queue->tailNode == NULL; // Seems that was the last node. Set the queue to the empty state
+	}
+
+	free(tempHead); // Delete the old node's data in memory.
+
+	return 0;
+
 }
 
 int queue_delete(queue_t queue, void *data)

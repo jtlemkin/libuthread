3ba1cf45f910dbd456bbbafece4d75627a8c0e74
Ntreynor <Ntreynor@gmail.com>
2020-11-08 14:54:04 -0800

    Progress made on uthread.c / phase 2

--- apps/queue_tester.c
+++ apps/queue_tester.c
@@ -18,467 +18,471 @@ do {									\
 /* Create */
 void test_create(void)
 {
-	fprintf(stderr, "*** TEST create ***\n");
+    fprintf(stderr, "*** TEST create ***\n");
 
-	TEST_ASSERT(queue_create() != NULL);
+    TEST_ASSERT(queue_create() != NULL);
 }
 /* Create Malloc Error */
 
 /* Destory Simple */
 void test_destroy(void)
 {
-	queue_t q;
-	int result;
+    queue_t q;
+    int result;
 
-	fprintf(stderr, "*** TEST destroy simple ***\n");
+    fprintf(stderr, "*** TEST destroy simple ***\n");
 
-	q = queue_create();
-	result = queue_destroy(q);
+    q = queue_create();
+    result = queue_destroy(q);
 
-	TEST_ASSERT(result == 0);
+    TEST_ASSERT(result == 0);
 }
 /* Destory Null */
 void test_destroy_null(void)
 {
-	int result;
+    int result;
 
-	fprintf(stderr, "*** TEST destroy null ***\n");
+    fprintf(stderr, "*** TEST destroy null ***\n");
 
-	result = queue_destroy(NULL);
+    result = queue_destroy(NULL);
 
-	TEST_ASSERT(result == -1);
+    TEST_ASSERT(result == -1);
 }
 /* Destroy Not Empty */
 void test_destroy_not_empty(void)
 {
-	queue_t q;
-	int result;
-	int t;
+    queue_t q;
+    int result;
+    int t;
 
-	fprintf(stderr, "*** TEST destroy not empty ***\n");
+    fprintf(stderr, "*** TEST destroy not empty ***\n");
 
-	q = queue_create();
-	queue_enqueue(q, &t);
-	result = queue_destroy(q);
+    q = queue_create();
+    queue_enqueue(q, &t);
+    result = queue_destroy(q);
 
-	// Should queue be deleted or not if not empty?
+    // Should queue be deleted or not if not empty?
 
-	TEST_ASSERT(result == -1);
+    TEST_ASSERT(result == -1);
 }
 
 /* Enqueue null */
 void test_enqueue_null(void)
 {
-	int result;
-	int data;
+    int result;
+    int data;
 
-	fprintf(stderr, "*** TEST enqueue null ***\n");
+    fprintf(stderr, "*** TEST enqueue null ***\n");
 
-	result = queue_enqueue(NULL, &data);
+    result = queue_enqueue(NULL, &data);
 
-	TEST_ASSERT(result == -1);
+    TEST_ASSERT(result == -1);
 }
 /* Enqueue data null */
 void test_enqueue_data_null(void)
 {
-	queue_t q;
-	int result;
+    queue_t q;
+    int result;
 
-	fprintf(stderr, "*** TEST enqueue data null ***\n");
+    fprintf(stderr, "*** TEST enqueue data null ***\n");
 
-	q = queue_create();
-	result = queue_enqueue(q, NULL);
+    q = queue_create();
+    result = queue_enqueue(q, NULL);
 
-	TEST_ASSERT(result == -1);
+    TEST_ASSERT(result == -1);
 }
 /* Enqueue malloc error */
 
 /* Dequeue null */
 void test_dequeue_null(void)
 {
-	int result;
-	int *ptr;
+    int result;
+    int *ptr;
 
-	fprintf(stderr, "*** TEST dequeue null ***\n");
+    fprintf(stderr, "*** TEST dequeue null ***\n");
 
-	result = queue_dequeue(NULL, (void**)&ptr);
+    result = queue_dequeue(NULL, (void**)&ptr);
 
-	TEST_ASSERT(result == -1);
+    TEST_ASSERT(result == -1);
 }
 /* Dequeue data pointer null */
 void test_dequeue_data_null(void)
 {
-	queue_t q;
-	int result;
+    queue_t q;
+    int result;
 
-	fprintf(stderr, "*** TEST dequeue data null ***\n");
+    fprintf(stderr, "*** TEST dequeue data null ***\n");
 
-	q = queue_create();
-	result = queue_dequeue(q, NULL);
+    q = queue_create();
+    result = queue_dequeue(q, NULL);
 
-	TEST_ASSERT(result == -1);
+    TEST_ASSERT(result == -1);
 }
 /* Dequeue empty */
 void test_dequeue_empty(void)
 {
-	queue_t q;
-	int result;
-	int *ptr;
+    queue_t q;
+    int result;
+    int *ptr;
 
-	fprintf(stderr, "*** TEST dequeue queue empty ***\n");
+    fprintf(stderr, "*** TEST dequeue queue empty ***\n");
 
-	q = queue_create();
-	result = queue_dequeue(q, (void**)&ptr);
+    q = queue_create();
+    result = queue_dequeue(q, (void**)&ptr);
 
-	TEST_ASSERT(result == -1);
+    TEST_ASSERT(result == -1);
 }
 
 /* Delete null */
 void test_delete_null(void)
 {
-	int result;
-	int data;
+    int result;
+    int data;
 
-	fprintf(stderr, "*** TEST delete queue null ***\n");
+    fprintf(stderr, "*** TEST delete queue null ***\n");
 
-	result = queue_delete(NULL, &data);
+    result = queue_delete(NULL, &data);
 
-	TEST_ASSERT(result == -1);
+    TEST_ASSERT(result == -1);
 }
 /* Delete data null */
 void test_delete_data_null(void)
 {
-	queue_t q;
-	int result;
+    queue_t q;
+    int result;
 
-	fprintf(stderr, "*** TEST delete data null ***\n");
-	q = queue_create();
+    fprintf(stderr, "*** TEST delete data null ***\n");
+    q = queue_create();
 
-	result = queue_delete(q, NULL);
+    result = queue_delete(q, NULL);
 
-	TEST_ASSERT(result == -1);
+    TEST_ASSERT(result == -1);
 }
 /* Delete data not in queue */
 void test_delete_not_found(void)
 {
-	int data[] = {1, 2, 3, 4, 5, 6};
-	queue_t q;
-	int i, result;
+    int data[] = {1, 2, 3, 4, 5, 6};
+    queue_t q;
+    int i, result;
 
-	fprintf(stderr, "*** TEST delete not found ***\n");
+    fprintf(stderr, "*** TEST delete not found ***\n");
 
-	q = queue_create();
-	for (i = 0; i < 5; ++i) {
-		queue_enqueue(q, data + i);
-	}
-	
-	result = queue_delete(q, data + 5);
+    q = queue_create();
+    for (i = 0; i < 5; ++i) {
+        queue_enqueue(q, data + i);
+    }
 
-	TEST_ASSERT(result == -1);
+    result = queue_delete(q, data + 5);
+
+    TEST_ASSERT(result == -1);
 }
 /* Delete success simple */
 void test_delete_simple(void)
 {
-	int data = 3;
-	queue_t q;
+    int data = 3;
+    queue_t q;
 
-	fprintf(stderr, "*** TEST delete simple ***\n");
+    fprintf(stderr, "*** TEST delete simple ***\n");
 
-	q = queue_create();
-	queue_enqueue(q, &data);
-	
-	queue_delete(q, &data);
+    q = queue_create();
+    queue_enqueue(q, &data);
 
-	TEST_ASSERT(queue_length(q) == 0);
+    queue_delete(q, &data);
+
+    TEST_ASSERT(queue_length(q) == 0);
 }
 /* Delete success complex */
 void test_delete_complex(void)
 {
-	int data[] = {1, 2, 3, 4, 5};
-	queue_t q;
-	int i, result;
+    int data[] = {1, 2, 3, 4, 5};
+    queue_t q;
+    int i, result;
 
-	fprintf(stderr, "*** TEST delete complex ***\n");
+    fprintf(stderr, "*** TEST delete complex ***\n");
 
-	q = queue_create();
-	for (i = 0; i < 5; ++i) {
-		queue_enqueue(q, data + i);
-	}
-	
-	result = queue_delete(q, data + 3);
+    q = queue_create();
+    for (i = 0; i < 5; ++i) {
+        queue_enqueue(q, data + i);
+    }
 
-	TEST_ASSERT(queue_length(q) == 4);
-	TEST_ASSERT(result == 0);
+    result = queue_delete(q, data + 3);
+
+    TEST_ASSERT(queue_length(q) == 4);
+    TEST_ASSERT(result == 0);
 }
 /* Delete success head */
 void test_delete_head(void)
 {
-	int data[] = {1, 2, 3};
-	queue_t q;
-	int i;
-	int *ptr;
+    int data[] = {1, 2, 3};
+    queue_t q;
+    int i;
+    int *ptr;
 
-	fprintf(stderr, "*** TEST delete head ***\n");
+    fprintf(stderr, "*** TEST delete head ***\n");
 
-	q = queue_create();
-	for (i = 0; i < 3; ++i) {
-		queue_enqueue(q, data + i);
-	}
-	
-	queue_delete(q, data);
-	TEST_ASSERT(queue_length(q) == 2);
+    q = queue_create();
+    for (i = 0; i < 3; ++i) {
+        queue_enqueue(q, data + i);
+    }
 
-	queue_dequeue(q, (void**)&ptr);
-	TEST_ASSERT(ptr == data + 1);
+    queue_delete(q, data);
+    TEST_ASSERT(queue_length(q) == 2);
+
+    queue_dequeue(q, (void**)&ptr);
+    TEST_ASSERT(ptr == data + 1);
 }
 
 /* Delete success tail */
 void test_delete_tail(void)
 {
-	int data[] = {1, 2, 3};
-	queue_t q;
-	int i;
-	int *ptr;
+    int data[] = {1, 2, 3};
+    queue_t q;
+    int i;
+    int *ptr;
 
-	fprintf(stderr, "*** TEST delete tail ***\n");
+    fprintf(stderr, "*** TEST delete tail ***\n");
 
-	q = queue_create();
-	for (i = 0; i < 3; ++i) {
-		queue_enqueue(q, data + i);
-	}
-	
-	queue_delete(q, data + 2);
-	TEST_ASSERT(queue_length(q) == 2);
+    q = queue_create();
+    for (i = 0; i < 3; ++i) {
+        queue_enqueue(q, data + i);
+    }
 
-	queue_dequeue(q, (void**)&ptr);
-	TEST_ASSERT(ptr == data);
+    queue_delete(q, data + 2);
+    TEST_ASSERT(queue_length(q) == 2);
+
+    queue_dequeue(q, (void**)&ptr);
+    TEST_ASSERT(ptr == data);
 }
 
 /* Iterate null */
 void inc(void *n) {
-	int *a = (int*) n;
-	*a = *a + 1;
+    int *a = (int*) n;
+    *a = *a + 1;
 }
 
 void test_iterate_null(void)
 {
-	int result;
+    int result;
 
-	fprintf(stderr, "*** TEST iterate queue null ***\n");
+    fprintf(stderr, "\n*** TEST iterate queue null ***\n\n");
 
-	result = queue_delete(NULL, &inc);
+    result = queue_delete(NULL, &inc);
 
-	TEST_ASSERT(result == -1);
+    TEST_ASSERT(result == -1);
 }
 /* Iterate function null */
 void test_iterate_function_null(void)
 {
-	queue_t q;
-	int result;
+    queue_t q;
+    int result;
 
-	fprintf(stderr, "*** TEST iterate function null ***\n");
-	q = queue_create();
+    fprintf(stderr, "\n*** TEST iterate function null ***\n\n");
+    q = queue_create();
 
-	result = queue_delete(q, NULL);
+    result = queue_delete(q, NULL);
 
-	TEST_ASSERT(result == -1);
+    TEST_ASSERT(result == -1);
 }
 /* Iterate simple */
 void test_iterate_simple(void)
 {
-	queue_t q;
-	int result;
-	int data[] = {1, 2, 3};
-	int i;
-	int *ptr;
+    queue_t q;
+    int result;
+    int data[] = {1, 2, 3};
+    int i;
+    int *ptr;
 
-	fprintf(stderr, "*** TEST iterate simple ***\n");
+    fprintf(stderr, "\n*** TEST iterate simple ***\n\n");
 
-	q = queue_create();
-	for (i = 0; i < 3; ++i) {
-		queue_enqueue(q, data + i);
-	}
-	queue_iterate(q, &inc);
-	for (i = 0; i < 3; ++i) {
-		result = queue_dequeue(q, (void**)&ptr);
-		TEST_ASSERT(result == 0);
-		// Each element should be incrememented by the inc function, each 
-		// element is also originally one greater than it's index, so the new 
-		// value should be i + 2
-		TEST_ASSERT(*ptr == i + 2);
-	}
+    q = queue_create();
+    for (i = 0; i < 3; ++i) {
+        queue_enqueue(q, data + i);
+    }
+    queue_iterate(q, &inc);
+    for (i = 0; i < 3; ++i) {
+        result = queue_dequeue(q, (void**)&ptr);
+        TEST_ASSERT(result == 0);
+        // Each element should be incrememented by the inc function, each
+        // element is also originally one greater than it's index, so the new
+        // value should be i + 2
+        TEST_ASSERT(*ptr == i + 2);
+    }
 }
 
 queue_t global_q;
 
 void delete_head(void* ptr) {
-	int *a = (int*) ptr;
-	if (*a == 1) {
-		queue_delete(global_q, a);
-	}
+    int *a = (int*) ptr;
+    if (*a == 1) {
+        queue_delete(global_q, a);
+    }
 }
 
 void delete_mid(void* ptr) {
-	int *a = (int*) ptr;
-	if (*a == 2) {
-		queue_delete(global_q, a);
-	}
+    int *a = (int*) ptr;
+    if (*a == 2) {
+        queue_delete(global_q, a);
+    }
 }
 
 void delete_tail(void* ptr) {
-	int *a = (int*) ptr;
-	if (*a == 3) {
-		queue_delete(global_q, a);
-	}
+    int *a = (int*) ptr;
+    if (*a == 3) {
+        queue_delete(global_q, a);
+    }
 }
 
 void delete_next(void* ptr) {
-	int *a = (int*) ptr;
-	if (*a == 1) {
-		queue_delete(global_q, a + 1);
-	}
+    int *a = (int*) ptr;
+    if (*a == 1) {
+        queue_delete(global_q, a + 1);
+    }
 }
 
 /* Iterate delete element */
 void test_iterate_delete(queue_func_t func)
 {
-	int result;
-	int data[] = {1, 2, 3};
-	int i, j;
-	int *ptr;
+    int result;
+    int data[] = {1, 2, 3};
+    int i, j;
+    int *ptr;
 
-	fprintf(stderr, "*** TEST iterate delete ***\n");
-	// Test each of the delete functions 
-	for (j = 0; j < 4; ++j) {
-		global_q = queue_create();
+    fprintf(stderr, "\n\n*** TEST iterate delete ***\n\n\n");
+    // Test each of the delete functions
+    for (j = 0; j < 4; ++j) {
+        global_q = queue_create();
 
-		for (i = 0; i < 3; ++i) {
-			queue_enqueue(global_q, data + i);
-		}
+        for (i = 0; i < 3; ++i) {
+            queue_enqueue(global_q, data + i);
+        }
 
-		queue_iterate(global_q, func);
-		
-		for (i = 0; i < 2; ++i) {
-			result = queue_dequeue(global_q, (void**)&ptr);
+        queue_iterate(global_q, func);
 
-			TEST_ASSERT(result == 0);
-		}
-		queue_destroy(global_q);
-	}
+        for (i = 0; i < 2; ++i) {
+            result = queue_dequeue(global_q, (void**)&ptr);
+
+            TEST_ASSERT(result == 0);
+        }
+        queue_destroy(global_q);
+    }
 }
 
 void test_iterate_delete_head(void) {
-	test_iterate_delete(&delete_head);
+    fprintf(stderr, "\n\n*** TEST iterate delete HEAD ***\n\n\n");
+    test_iterate_delete(&delete_head);
 }
 
-void test_iterate_delete_tail(void) {
-	test_iterate_delete(&delete_tail);
+void test_iterate_delete_tail(void){
+    fprintf(stderr, "\n\n*** TEST iterate delete TAIL ***\n\n\n");
+    test_iterate_delete(&delete_tail);
 }
 
 void test_iterate_delete_mid(void) {
-	test_iterate_delete(&delete_mid);
+    fprintf(stderr, "\n\n*** TEST iterate delete MID ***\n\n\n");
+    test_iterate_delete(&delete_mid);
 }
 
 void test_iterate_delete_next(void) {
-	test_iterate_delete(&delete_next);
+    fprintf(stderr, "\n\n*** TEST iterate delete NEXT ***\n\n\n");
+    test_iterate_delete(&delete_next);
 }
 
 /* Length null */
 void test_length_null(void)
 {
-	int result;
+    int result;
 
-	fprintf(stderr, "*** TEST length null ***\n");
+    fprintf(stderr, "*** TEST length null ***\n");
 
-	result = queue_length(NULL);
+    result = queue_length(NULL);
 
-	TEST_ASSERT(result == -1);
+    TEST_ASSERT(result == -1);
 }
 
 /* Length simple */
 void test_length_simple(void)
 {
-	queue_t q;
-	int data[] = {1, 2, 3};
-	int i;
+    queue_t q;
+    int data[] = {1, 2, 3};
+    int i;
 
-	fprintf(stderr, "*** TEST length simple ***\n");
+    fprintf(stderr, "*** TEST length simple ***\n");
 
-	q = queue_create();
-	for (i = 0; i < 3; ++i) {
-		queue_enqueue(q, data + i);
-	}
-	TEST_ASSERT(queue_length(q) == 3);
+    q = queue_create();
+    for (i = 0; i < 3; ++i) {
+        queue_enqueue(q, data + i);
+    }
+    TEST_ASSERT(queue_length(q) == 3);
 }
 
 /* Enqueue/Dequeue simple */
 void test_queue_simple(void)
 {
-	int data = 3, *ptr;
-	queue_t q;
-	int result1, result2;
+    int data = 3, *ptr;
+    queue_t q;
+    int result1, result2;
 
-	fprintf(stderr, "*** TEST queue_simple ***\n");
+    fprintf(stderr, "*** TEST queue_simple ***\n");
 
-	q = queue_create();
-	result1 = queue_enqueue(q, &data);
-	result2 = queue_dequeue(q, (void**)&ptr);
-	TEST_ASSERT(result1 == 0);
-	TEST_ASSERT(result2 == 0);
-	TEST_ASSERT(ptr == &data);
+    q = queue_create();
+    result1 = queue_enqueue(q, &data);
+    result2 = queue_dequeue(q, (void**)&ptr);
+    TEST_ASSERT(result1 == 0);
+    TEST_ASSERT(result2 == 0);
+    TEST_ASSERT(ptr == &data);
 }
 
 /* Enqueue/Dequeue complex */
 void test_queue_complex(void)
 {
-	int data[5] = {1, 2, 3, 4, 5};
-	int *ptr;
-	queue_t q;
-	int i;
+    int data[5] = {1, 2, 3, 4, 5};
+    int *ptr;
+    queue_t q;
+    int i;
 
-	fprintf(stderr, "*** TEST queue_simple ***\n");
+    fprintf(stderr, "*** TEST queue_simple ***\n");
 
-	q = queue_create();
-	for (i = 0; i < 5; ++i) {
-		queue_enqueue(q, (data + i));
-	}
-	for (i = 0; i < 5; ++i) {
-		queue_dequeue(q, (void**)&ptr);
-		TEST_ASSERT(ptr == (data + i));
-	}
+    q = queue_create();
+    for (i = 0; i < 5; ++i) {
+        queue_enqueue(q, (data + i));
+    }
+    for (i = 0; i < 5; ++i) {
+        queue_dequeue(q, (void**)&ptr);
+        TEST_ASSERT(ptr == (data + i));
+    }
 }
 
 
 int main(void)
 {
-	test_create();
-	test_destroy();
-	test_destroy_null();
-	test_destroy_not_empty();
-	test_enqueue_null();
-	test_enqueue_data_null();
-	test_dequeue_null();
-	test_dequeue_data_null();
-	test_dequeue_empty();
-	test_delete_null();
-	test_delete_data_null();
-	test_delete_not_found();
-	test_delete_simple();
-	test_delete_complex();
-	test_delete_head();
-	test_delete_tail();
-	test_iterate_null();
-	test_iterate_function_null();
-	test_iterate_simple();
-	test_iterate_function_null();
-	test_iterate_delete_head();
-	test_iterate_delete_tail();
-	test_iterate_delete_mid();
-	test_iterate_delete_next();
-	test_length_null();
-	test_length_simple();
-	test_queue_simple();
-	test_queue_complex();
+    test_create();
+    test_destroy();
+    test_destroy_null();
+    test_destroy_not_empty();
+    test_enqueue_null();
+    test_enqueue_data_null();
+    test_dequeue_null();
+    test_dequeue_data_null();
+    test_dequeue_empty();
+    test_delete_null();
+    test_delete_data_null();
+    test_delete_not_found();
+    test_delete_simple();
+    test_delete_complex();
+    test_delete_head();
+    test_delete_tail();
+    test_iterate_null();
+    test_iterate_function_null();
+    test_iterate_simple();
+    test_iterate_function_null();
+    test_iterate_delete_head();
+    test_iterate_delete_tail();
+    test_iterate_delete_mid();
+    test_iterate_delete_next();
+    test_length_null();
+    test_length_simple();
+    test_queue_simple();
+    test_queue_complex();
 
-	return 0;
+    return 0;
 }
--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -8,19 +8,63 @@
 
 #include "private.h"
 #include "uthread.h"
+#include "queue.h"
 
+// General idea. Create a queue of thread objects.
+
+#define STACK_SIZE 32768
+
+typedef struct uthread_tcb* uthread_tcb_t;
+
+uthread_tcb_t* currTCB;
+
+static uthread_ctx_t mainContext[1];
+struct queue_t globalQueue;
 struct uthread_tcb {
 	/* TODO Phase 2 */
+
+	/*
+	 * An example of information contained within a TCB is:
+
+Thread Identifier: Unique id (tid) is assigned to every new thread
+Stack pointer: Points to thread's stack in the process
+Program counter: Points to the current program instruction of the thread
+State of the thread (running, ready, waiting, start, done)
+Thread's register values
+Pointer to the Process control block (PCB) of the process that the thread lives on
+
+	 */
+
+	// A state
+	// A backup of registers
+	// a stack.
+	int state;
+	uthread_ctx_t *uctx;
+    void *top_of_stack;
 };
 
 struct uthread_tcb *uthread_current(void)
 {
 	/* TODO Phase 2 */
+	return currTCB;
 }
 
 void uthread_yield(void)
 {
 	/* TODO Phase 2 */
+
+	// General idea: Dequeue the next thread on the
+
+    struct uthread_tcb* nextThread;
+    queue_dequeue(globalQueue, (void**) &nextThread);
+
+    uthread_tcb_t* currTCBPointer = &currTCB;
+    queue_enqueue(globalQueue, currTCBPointer);
+
+
+
+    //uthread_ctx_switch(uthread_current()->uctx, currThread->uctx);
+
 }
 
 void uthread_exit(void)
@@ -28,14 +72,74 @@ void uthread_exit(void)
 	/* TODO Phase 2 */
 }
 
+uthread_tcb* tcb_create(uthread_func_t func, void *arg)
+{
+
+    uthread_tcb* z = (struct uthread_tcb*) malloc(sizeof(struct struct uthread_tcb));
+
+    z->top_of_stack = *uthread_ctx_alloc_stack(void);
+    z->state = 0;
+    z->uctx = (uthread_ctx_t*) malloc(sizeof(uthread_ctx_t));
+
+    getcontext(z->uctx);
+    z->uctx->uc_stack.ss_sp   = z->top_of_stack;
+    z->uctx->uc_stack.ss_size = STACK_SIZE;
+    makecontext(z->uctx, (func_t)func, arg);
+
+    return z;
+}
+
 int uthread_create(uthread_func_t func, void *arg)
 {
 	/* TODO Phase 2 */
+	uthread_tcb* tempStruct = tcb_create(func, arg);
+	if (tempStruct == NULL){
+	    return -1;
+	}
+	queue_enqueue(globalQueue, tempStruct);
+	return 0;
+
+	// Create a thread control block
+	// Create a stack using *uthread_ctx_alloc_stack
+    // Contain a pointer to
+}
+
+int tcb_free(uthread_tcb_t structToFree)
+{
+    // free up the thread control block object
 }
 
 int uthread_start(uthread_func_t func, void *arg)
 {
 	/* TODO Phase 2 */
+    globalQueue = queue_create();
+    if (uthread_create(idleFunc, void) == -1){ //
+        return -1;
+    }
+    if (uthread_create(func, arg) == -1){ // Create the "main" (second) thread.
+        return -1;
+    }
+
+    queue_dequeue(globalQueue, (void**) &currTcb);
+    uthread_ctx_switch(&mainContext[0], &(*currThread.uctx)); // Something like that.
+    // swapcontext(&mainContext[0], &ctx[to]);
+
+    // THIS
+    return 0;
+    // Create a thread with which to place in the queue. using uthread_create
+    //queue_enqueue(GlobalQueue, void *data) enqueue the intial thread into the queue.
+
+	// Initialize the global queue of uthread_tcb objects
+	// create the intitial
+}
+
+int idleFunc(void){
+    while (queue_length(globalQueue) > 1){
+        uthread_yield();
+        // handle threads with completed execution and terminate their TCB
+    }
+    // Perform context switch to original thread
+    return 0;
 }
 
 void uthread_block(void)

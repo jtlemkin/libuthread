39c80ce69ec58c38474a40206963226fbe7b10ed
Ntreynor <Ntreynor@gmail.com>
2020-11-12 22:04:48 -0800

    Fixed preemption insanity with "volatile"

--- apps/preempt_two_threads.c
+++ apps/preempt_two_threads.c
@@ -11,21 +11,21 @@
 
 #include <uthread.h>
 
-int *is_done;
+volatile int is_done; // Needs to be declared volotile to prevent compiler optimizations from preventing the while loop on line 38 from running.
 
 void thread2()
 {
     fprintf(stderr, "*** Thread 2 initiate ***\n");
-    fprintf(stderr, "*** Thread 2 value of is_done: %d***\n", *is_done);
+    fprintf(stderr, "*** Thread 2 value of is_done: %d***\n", is_done);
     for (int j = 0; j < 5; ++j) {
-        fprintf(stderr, "*** Thread 2 inside loop, about to yield ***\n");
+        fprintf(stderr, "*** Thread 2 inside loop iteration %d, about to yield ***\n", j);
         uthread_yield();
-        fprintf(stderr, "*** Thread 2 resumes ***\n");
+        fprintf(stderr, "*** Thread 2 resumes from iteration %d ***\n", j);
     }
     fprintf(stderr, "*** Thread 2 concluding loop ***\n");
 
-    *is_done = 1;
-    fprintf(stderr, "*** Thread 2 value of is_done: %d***\n", *is_done);
+    is_done = 1;
+    fprintf(stderr, "*** Thread 2 value of is_done: %d***\n", is_done);
     fprintf(stderr, "*** Thread 2 sets value to terminate other thread. ***\n");
 }
 
@@ -34,22 +34,18 @@ void thread1()
     fprintf(stderr, "*** Thread 1 initiate ***\n");
     uthread_create(thread2, NULL);
     fprintf(stderr, "*** Thread 1 about to enter infinite loop ***\n");
-    fprintf(stderr, "*** Thread 1 value of is_done: %d***\n", *is_done);
-    int localVal = *is_done;
-    while (!localVal){
-        localVal = *is_done;
-        fprintf(stderr, "*** Thread 1 value of is_done: %d***\n", *is_done);
+    while (!is_done){
+        // fprintf(stderr, "T1 is_done: %d\n", *is_done);
+        // No need to flood the output with content. This is an infinite loop that we can only excape with preemeption.
     }
     fprintf(stderr, "*** Thread 1 concludes due to preemption causing thread 2 to run and change condition to exit while loop ***\n");
 }
 
 int main(void)
 {
-    is_done = malloc(sizeof(int));
-    *is_done = 0;
+    is_done = 0;
     fprintf(stderr, "*** Main Start ***\n");
     uthread_start(thread1, NULL);
     fprintf(stderr, "*** Main Finish ***\n");
-    free(is_done);
     return 0;
 }
